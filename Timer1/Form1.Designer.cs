9d2d2b0832f16b723789da6630b8225222529e47
zealot <savichpg@yahoo.com>
2020-07-19 18:34:28 +0300

    Альфа-версия проекта

---
+++ Timer1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Timer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.hours = new System.Windows.Forms.NumericUpDown();
+            this.clock = new System.Windows.Forms.Label();
+            this.minutes = new System.Windows.Forms.NumericUpDown();
+            this.seconds = new System.Windows.Forms.NumericUpDown();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hours
+            // 
+            this.hours.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hours.ForeColor = System.Drawing.Color.Red;
+            this.hours.Location = new System.Drawing.Point(275, 124);
+            this.hours.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(74, 44);
+            this.hours.TabIndex = 1;
+            this.hours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clock.ForeColor = System.Drawing.Color.Lime;
+            this.clock.Location = new System.Drawing.Point(191, 13);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(417, 108);
+            this.clock.TabIndex = 2;
+            this.clock.Text = "00:00:00";
+            // 
+            // minutes
+            // 
+            this.minutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minutes.ForeColor = System.Drawing.Color.Red;
+            this.minutes.Location = new System.Drawing.Point(355, 124);
+            this.minutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minutes.Name = "minutes";
+            this.minutes.Size = new System.Drawing.Size(74, 44);
+            this.minutes.TabIndex = 3;
+            this.minutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // seconds
+            // 
+            this.seconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.seconds.ForeColor = System.Drawing.Color.Red;
+            this.seconds.Location = new System.Drawing.Point(435, 124);
+            this.seconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.seconds.Name = "seconds";
+            this.seconds.Size = new System.Drawing.Size(74, 44);
+            this.seconds.TabIndex = 4;
+            this.seconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(595, 23);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(87, 71);
+            this.start.TabIndex = 5;
+            this.start.Text = "ON";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(595, 100);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(87, 71);
+            this.stop.TabIndex = 6;
+            this.stop.Text = "OFF";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Timer1.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(698, 188);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.seconds);
+            this.Controls.Add(this.minutes);
+            this.Controls.Add(this.clock);
+            this.Controls.Add(this.hours);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Простой таймер с функцией будильника";
+            ((System.ComponentModel.ISupportInitialize)(this.hours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown hours;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.NumericUpDown minutes;
+        private System.Windows.Forms.NumericUpDown seconds;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Timer1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Timer1
+{
+    public partial class Form1 : Form
+    {
+        private int h;
+        private int m;
+        private int s;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayCurrentTime();
+            h = m = s = 0;
+        }
+
+        private void DisplayCurrentTime()
+        {
+            DateTime dt = DateTime.Now;
+            clock.Text = dt.ToLongTimeString();
+            if (dt.Hour == h && dt.Minute == m && dt.Second == s)
+            {
+                //MessageBox.Show("Время истекло!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Process.Start(@"..\..\Audio\audio.mp3");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Ku-ku", "Test", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            DisplayCurrentTime();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            h = (int)hours.Value;
+            m = (int)minutes.Value;
+            s = (int)seconds.Value;
+            
+            //hours.ForeColor = Color.Red;
+            //minutes.ForeColor = Color.Red;
+            //seconds.ForeColor = Color.Red;
+
+            hours.Enabled = false;
+            minutes.Enabled = false;
+            seconds.Enabled = false;
+
+            MessageBox.Show("Будильник включен!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            h = m = s = 0;
+
+            //hours.ForeColor = Color.MediumBlue;
+            //minutes.ForeColor = Color.MediumBlue;
+            //seconds.ForeColor = Color.MediumBlue;
+
+            hours.Enabled = true;
+            minutes.Enabled = true;
+            seconds.Enabled = true;
+
+            MessageBox.Show("Будильник выключен!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
